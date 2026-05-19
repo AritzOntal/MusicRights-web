d69a4ee1533ec861a933131255dde47cede747ad
Aritz Ontalvilla <aritzontal@gmail.com>
2026-05-19 12:12:11 +0200

    añadiod servido de creacion de obras

--- src/services/workServices.ts
+++ src/services/workServices.ts
@@ -10,3 +10,11 @@ export async function getWorkById(id: number): Promise<Work> {
     const response = await apiClient.get<Work>(`/v1/works/${id}`)
     return response.data
 }
+
+// Omitimos ide porque lo genera el Back
+export type NewWork = Omit<Work, 'id'>
+
+export async function createWork(work: NewWork): Promise<Work> {
+    const response = await apiClient.post<Work>('/v1/works', work)
+    return response.data
+}
