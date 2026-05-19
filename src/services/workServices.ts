9ae0223a29d58289bba33560c98238dd5650b21e
Aritz Ontalvilla <aritzontal@gmail.com>
2026-05-19 11:54:28 +0200

    Añadido servicio de obra por id

--- src/services/workServices.ts
+++ src/services/workServices.ts
@@ -5,3 +5,8 @@ export async function getAllWorks(): Promise<Work[]> {
     const response = await apiClient.get<Work[]>('/v1/works')
     return response.data
 }
+
+export async function getWorkById(id: number): Promise<Work> {
+    const response = await apiClient.get<Work>(`/v1/works/${id}`)
+    return response.data
+}
